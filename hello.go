be9e951a573cf4a770089fe6a6e3732a141b058e
Bruno Quindeler <bruno.quindeler@db1.com.br>
2021-08-30 17:09:17 -0300

    feat: added ReverseRunes Function

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Brunoquindeler/hello/morestrings"
+)
 
 func main() {
-	fmt.Println("Hello, World.")
+	fmt.Println(morestrings.ReverseRunes("Hello, World!"))
 }
