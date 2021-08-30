c6c1e27c1ed7af26bd792ef70cc4b3b37850a523
Bruno Quindeler <bruno.quindeler@db1.com.br>
2021-08-30 17:18:23 -0300

    feat: importing packages from remote modules

--- hello.go
+++ hello.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/Brunoquindeler/hello/morestrings"
+	"github.com/google/go-cmp/cmp"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("Hello, World!"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
